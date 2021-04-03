fa8449780b08be9613600df57f062b022f7ce4e6
sou_devlop <sou.business0109@gmail.com>
2021-04-03 11:59:17 +0900

    #2_Search User_Resource And Tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,21 @@
 module Api
     module V1
         class UsersController < ApplicationController
+            attr_reader :create_user, :search_user
+
             def initialize
                 @create_user = CreateUserAppService.new()
+                @search_user = SearchUserAppService.new()
                 super
             end
 
             def create
                 responce = @create_user.execute(req_post_params)
+                render json: responce
+            end
+
+            def index
+                responce = @search_user.execute(params[:id])
                 render json: responce
             end
 
